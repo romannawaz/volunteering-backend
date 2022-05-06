fb72fb42ae1918ea3b76716e40258e15b52f54cc
romannawaz <romannawaz01@gmail.com>
2022-05-06 17:32:26 +0300

    feat: connection to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Mongoose
+import { MongooseModule } from '@nestjs/mongoose';
+
+const _username: string = 'romannawaz';
+const _password: string = 'nshn8d-134';
+const mongoUri: string = `mongodb+srv://${_username}:${_password}@cluster0.f2lh8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoUri)],
   controllers: [AppController],
   providers: [AppService],
 })
